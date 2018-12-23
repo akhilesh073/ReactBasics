899aecafc39b5ecbf4dcdc76f0a066d9b664b809
amohapatro <Akhilesh_Mohapatro@intuit.com>
2018-12-23 10:17:34 +0530

    Second commit

---
+++ SecondComponent.jsx
@@ -0,0 +1,41 @@
+class Button extends React.Component {
+
+    handleClick = () => {
+        this.props.onClickFunction(this.props.increment);
+    };
+    render() {
+        return (
+            <button onClick={this.handleClick}> + {this.props.increment}
+            </button>
+        );
+    };
+}
+
+const Result = (props) => {
+    return (<div>{props.counter}</div>);
+};
+
+class App extends React.Component {
+
+    state = {
+        counter: 0
+    };
+
+    incrementCounter = (increment) => {
+        this.setState((prevState) => ({ counter: prevState.counter + increment }));
+    };
+
+    render() {
+        return (
+            <div>
+                <Button increment={1} onClickFunction={this.incrementCounter} />
+                <Button increment={5} onClickFunction={this.incrementCounter} />
+                <Button increment={10} onClickFunction={this.incrementCounter} />
+                <Button increment={100} onClickFunction={this.incrementCounter} />
+                <Result counter={this.state.counter}></Result>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, mountNode);
